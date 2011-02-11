c50ccb2b6e635bc28a1ae33e284d472f5b2dabe9
Djalma Oliveira <djalmaoliveira@gmail.com>
2011-02-11 13:01:53 -0300

    add: admin "Loading..." text;

--- app/app/assets/js/admin.js
+++ app/app/assets/js/admin.js
@@ -4,17 +4,18 @@ function loadPage(page, query, method) {
     if ( !area ) {
         area = document.createElement('div');
         $(area).attr('id', "loading_layer");
-        texto = document.createElement('div');
+        text = document.createElement('div');
         $(text).attr('id', "text_layer");
     }
-    var t = 20;
-    var h = 500;
-    var l = 20;
-    var w = 800;
+    var t = $('#main').offset().top;
+	var D = document;
+	var h = -t + Math.max(Math.max(D.body.scrollHeight, D.documentElement.scrollHeight), Math.max(D.body.offsetHeight, D.documentElement.offsetHeight), Math.max(D.body.clientHeight, D.documentElement.clientHeight));
+	var l = $('#main').offset().left;
+	var w = Math.max(Math.max(D.body.scrollWidth, D.documentElement.scrollWidth),Math.max(D.body.offsetWidth, D.documentElement.offsetWidth),Math.max(D.body.clientWidth, D.documentElement.clientWidth));
 
     $(area).css({'display':'none','opacity':'0.7','height':h,'width':w,'position':'absolute','text-align':'center','color':'white','font-size':'1.5em',  'margin':'auto','z-index':'998', 'top':t,'left':l});
-    $(text).css({'display':'none','height':h,'width':w,'position':'absolute','text-align':'center','color':'white','font-size':'1.5em', 'font-weight':'bold', 'padding-top':'50px', 'margin':'auto','z-index':'999', 'top':t,'left':l});
-    $(text).html('...');
+    $(text).css({'display':'none','height':h-50,'width':w-50,'position':'absolute','text-align':'center','color':'white','font-size':'1.5em', 'font-weight':'bold', 'padding-top':'50px', 'margin':'auto','z-index':'999', 'top':t,'left':l+25});
+    $(text).html('Loading...');
     $(document.body).append(area);
     $(document.body).append(text);
     $(area).fadeIn('slow');
@@ -22,12 +23,12 @@ function loadPage(page, query, method) {
 
     if (method == 'post') {
         $.post(page, query, function(resp) {
-            $("#main_area").html(resp);
+            $("#main").html(resp);
             $(area).remove();
             $(text).remove();
         })
     } else {
-        $("#main_area").load(page, query, function (data) {
+        $("#main").load(page, query, function (data) {
             $(area).remove();
             $(text).remove();
         });
--- app/www/index.php
+++ app/www/index.php
@@ -2,6 +2,14 @@
 include "../app/config/config.php";
 include $aConfigs["configs"]["BASE_PATH"]."Liber.php";
 Liber::loadConfig($aConfigs);
+
+// avoid cache
+header("Pragma: public");
+header("Expires: -1");
+header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+header("Cache-Control: private",false);
+header("Cache-Control: no-cache");
+
 Liber::run();
 
 ?>
