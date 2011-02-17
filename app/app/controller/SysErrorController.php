2a1f04e829a1e6b245cd0bd162a16105ffa1b427
Djalma Oliveira <djalmaoliveira@gmail.com>
2011-02-17 16:28:54 -0300

    add: Friendly system error page and send a warning mail to contact_email.

--- app/app/controller/SysErrorController.php
+++ app/app/controller/SysErrorController.php
@@ -1,13 +1,32 @@
 <?php
 
-class SysErrorController {
+class SysErrorController extends Controller {
 
     public function index() {
-        echo "system error"    ;
+        header('HTTP/1.0 500 Not Found');
 
+
+		$this->view()->template()->load('syserror.html', Array());
+		$oConfig = Liber::loadModel('Config', true);
+		$oMail = Liber::loadClass("Mailer",true);
+		$oMail->to($oConfig->data('contact_email'));
+		$oMail->from($oConfig->data('contact_email'));
+		$oMail->subject("Warning from ".Liber::conf('APP_URL'));
+		$oMail->html(true);
+		$msg = "
+
+			<strong>LiberSB found a problem.</strong>
+			<br/>
+			<strong>Where:</strong> ".url_current_(true)."
+			<br/>
+			<strong>When:</strong> ".date('Y-m-d H:i:s')."
+			<hr/>
+			<pre>".Liber::log()->toString()."</pre>
+
+
+		";
+		$oMail->body($msg);
+		$oMail->send();
     }
-	
-
 }
 ?>
-
