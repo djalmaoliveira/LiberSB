644873bbeb9676daac1caf3e1a35759bacbb342c
Djalma Oliveira <djalmaoliveira@gmail.com>
2011-03-01 16:06:47 -0300

    add: Share buttons on content.

--- app/app/helper/Content.php
+++ app/app/helper/Content.php
@@ -23,13 +23,34 @@ function content_url_($aContent, $return=false) {
 function content_show_($aContent, $isSummary=true, $return=false) {
     if ( !$aContent ) { return; }
 	$url = content_url_($aContent, true);
+	html_script_("			(function() {
+				var s = document.createElement('SCRIPT'), s1 = document.getElementsByTagName('SCRIPT')[0];
+				s.type = 'text/javascript';
+				s.async = true;
+				s.src = 'http://widgets.digg.com/buttons.js';
+				s1.parentNode.insertBefore(s, s1);
+				})();
+	");
     $html = "
         <div class=\"content_box\">
             <h3><a href='".$url."'>".$aContent['title']."</a></h3>
             ".date('Y/m/d',strtotime($aContent['create_datetime']))."
             <br/><br/>
             <div class='content_body'>".($isSummary?strip_tags($aContent['body'])."...<br/> <a href=\"$url\" title=\"see the entire content\"> read more...</a>":$aContent['body'])." </div>
+			<br/>
+
+			<div class='share_box' title='Share this content'>
+				<span><iframe height=\"20px\" width=\"100px\" scrolling=\"no\" frameborder=\"0\"  allowTransparency=\"true\" style=\"border:none; overflow:hidden;\"
+					src=\"http://www.facebook.com/plugins/like.php?href=$url&with=100&layout=button_count&show_faces=false&action=recommend&colorscheme=light\" ></iframe>
+				</span>
+				<span><a class=\"DiggThisButton DiggCompact\" href=\"http://digg.com/submit?style=no\"></a></span>
+				<span><iframe allowtransparency=\"true\" height=\"20px\" width=\"100px\" frameborder=\"0\" scrolling=\"no\"
+					src=\"http://platform.twitter.com/widgets/tweet_button.html\"></iframe>
+				</span>
+			</div>
+
             <div class=\"cleaner\"></div>
+			<br/>
             <div id=\"comment_nav\">
                 <a href='javascript:void(0)' onmouseover=\"leaveComment('".url_to_('/comment', true)."?content_id=".$aContent['content_id']."', this)\">Leave a comment</a>
                 <a href='".content_comment_url_($aContent)."'>See comments</a>
