44d6fa07130613d59650395fd88ec7958eac8837
Djalma Oliveira <djalmaoliveira@gmail.com>
2011-09-15 07:56:08 -0300

    fix: on syserror controller didn't work.

--- app/app/controller/SysErrorController.php
+++ app/app/controller/SysErrorController.php
@@ -3,30 +3,31 @@
 class SysErrorController extends Controller {
 
     public function index() {
+        Liber::loadHelper('HTML');
         header('HTTP/1.0 500 Not Found');
 
 
-		$this->view()->template()->load('syserror.html', Array());
-		$oConfig = Liber::loadModel('Config', true);
-		$oMail = Liber::loadClass("Mailer",true);
-		$oMail->to($oConfig->data('contact_email'));
-		$oMail->from($oConfig->data('contact_email'));
-		$oMail->subject("Warning from ".Liber::conf('APP_URL'));
-		$oMail->html(true);
-		$msg = "
+        $this->view()->template()->load('syserror.html', Array());
+        $oConfig = Liber::loadModel('Config', true);
+        $oMail = Liber::loadClass("Mailer",true);
+        $oMail->to($oConfig->data('contact_email'));
+        $oMail->from($oConfig->data('contact_email'));
+        $oMail->subject("Warning from ".Liber::conf('APP_URL'));
+        $oMail->html(true);
+        $msg = "
 
-			<strong>LiberSB found a problem.</strong>
-			<br/>
-			<strong>Where:</strong> ".url_current_(true)."
-			<br/>
-			<strong>When:</strong> ".date('Y-m-d H:i:s')."
-			<hr/>
-			<pre>".Liber::log()->toString()."</pre>
+            <strong>LiberSB found a problem.</strong>
+            <br/>
+            <strong>Where:</strong> ".url_current_(true)."
+            <br/>
+            <strong>When:</strong> ".date('Y-m-d H:i:s')."
+            <hr/>
+            <pre>".Liber::log()->toString()."</pre>
 
 
-		";
-		$oMail->body($msg);
-		$oMail->send();
+        ";
+        $oMail->body($msg);
+        $oMail->send();
     }
 }
 ?>
